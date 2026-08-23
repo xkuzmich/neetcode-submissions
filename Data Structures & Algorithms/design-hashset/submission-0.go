0f128b198cf77ec8997a41e73fd9e72558bbaff0
Andrei Kuzmich <38387408+xkuzmich@users.noreply.github.com>
2026-08-23 15:46:51 +0200

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.go
@@ -0,0 +1,50 @@
+const numBuckets = 1024
+
+type MyHashSet struct {
+	buckets [][]int
+}
+
+func Constructor() MyHashSet {
+    return MyHashSet{buckets: make([][]int, numBuckets)}
+}
+
+func (this *MyHashSet) hash(key int) int {
+	return key & (numBuckets - 1)
+}
+func (this *MyHashSet) Add(key int) {
+    bucket := this.hash(key)
+	if !this.Contains(key) {
+		this.buckets[bucket] = append(this.buckets[bucket], key)
+	}
+	
+}
+
+func (this *MyHashSet) Remove(key int) {
+   bucket := this.hash(key)
+   	for i, k := range this.buckets[bucket] {
+		if k == key {
+			this.buckets[bucket] = append(this.buckets[bucket][:i],this.buckets[bucket][i + 1:]...) 
+			break
+		}
+	}
+	
+}
+
+func (this *MyHashSet) Contains(key int) bool {
+	bucket := this.hash(key)
+	for _, k := range this.buckets[bucket] {
+		if k == key {
+			return true
+		}
+	}
+    return false
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * param_3 := obj.Contains(key);
+ */
+ 
