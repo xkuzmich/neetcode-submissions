495817080e2d3564e267ba8540989b0eeaabe6e2
Andrei Kuzmich <38387408+xkuzmich@users.noreply.github.com>
2026-08-24 21:10:18 +0200

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.go
@@ -0,0 +1,54 @@
+const numBuckets = 1024
+
+type MyHashMap struct {
+	buckets [][][]int
+}
+
+func Constructor() MyHashMap {
+    return MyHashMap{buckets: make([][][]int, numBuckets)}
+}
+
+func (this *MyHashMap) hash(key int) int {
+	return key & (numBuckets - 1)
+}
+
+func (this *MyHashMap) Put(key int, value int) {
+    bucket := this.hash(key)
+	for _, k := range this.buckets[bucket] {
+		if k[0] == key {
+			k[1] = value
+			return
+		}
+	}
+	this.buckets[bucket] = append(this.buckets[bucket], []int{key, value})
+}
+
+func (this *MyHashMap) Get(key int) int {
+    bucket := this.hash(key)
+	for _, k := range this.buckets[bucket] {
+		if k[0] == key {
+			return k[1]
+		}
+	}
+	return -1
+}
+
+func (this *MyHashMap) Remove(key int) {
+    bucket := this.hash(key)
+   	for i, k := range this.buckets[bucket] {
+		if k[0] == key {
+			b := this.buckets[bucket]
+			b[i] = b[len(b)-1]
+			this.buckets[bucket] = b[:len(b)-1]
+			break
+		}
+	}
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Put(key,value);
+ * param_2 := obj.Get(key);
+ * obj.Remove(key);
+ */
